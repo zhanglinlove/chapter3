0ae31a24f7cb079e44a2757c534a03229cb83364
zhanglin <525975661@qq.com>
2018-08-27 11:48:01 +0800

    提交chapter3项目

---
+++ src/main/java/org/smart4j/chapter3/Constant/ConfigConstant.java
@@ -0,0 +1,13 @@
+package org.smart4j.chapter3.Constant;
+
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "smart.properties";
+    String JDBC_DRIVER = "smart.frameworker.jdbc.driver";
+    String JDBC_URL = "smart.frameworker.jdbc.url";
+    String JDBC_USERNAME = "smart.frameworker.jdbc.username";
+    String JDBC_PASSWORD = "smart.frameworker.jdbc.password";
+    String APP_BASE_PACKAG = "smart.frameworker.app.base_package";
+    String APP_JSP_PATH = "smart.frameworker.app.jsp_path";
+    String APP_ASSET_PATH = "smart.frameworker.app.asset_path";
+}
